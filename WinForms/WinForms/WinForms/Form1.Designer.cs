e1243e2ad8d7084155677dbb2e430b73cb8bb67f
Austin Marino <marino_austin@yahoo.com>
2018-09-12 21:16:25 -0700

    Completed all code necessary fort WinForms

---
+++ WinForms/WinForms/WinForms/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows.Forms;
+
+
+namespace WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(827, 450);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Enter += new System.EventHandler(this.Form1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 450);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Austin Marino - 11507852";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+    }
+}
+
---
+++ WinForms/WinForms/WinForms/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < 10000; i++)
+            {
+                numbers.Add(rand.Next(0, 20001));
+            }
+            //All times are in Milliseconds
+            int mapCount = this.UnorderedMap(numbers, out double mapTime);
+            int constMemCount = this.ConstantMemoryComplexity(numbers, out double constMemTime);
+            int listCount = this.ListMethods(numbers, out double listTime);
+            StringBuilder results = new StringBuilder();
+            results.Append("Hash-Set Method:\r\n"
+                + $"  ~ Unique Numbers: {mapCount}\r\n"
+                + $"  ~ Time (Milliseconds): {mapTime}\r\n"
+                + "  ~ Time Complexity: O(n)\r\n"
+                + "  ~ Memory Complexity: O(n)\r\n\r\n");
+            results.Append("Constant Storage Method:\r\n"
+                + $"  ~ Unique Numbers: {constMemCount}\r\n"
+                + $"  ~ Time (Milliseconds): {constMemTime}\r\n"
+                + "  ~ Time Complexity: O(n^2)\r\n"
+                + "  ~ Memory Complexity: O(1)\r\n\r\n");
+            results.Append("Sorted Method:\r\n"
+                + $"  ~ Unique Numbers: {listCount}\r\n"
+                + $"  ~ Time (Milliseconds): {listTime}\r\n"
+                + "  ~ Time Complexity: O(n)\r\n"
+                + "  ~ Memory Complexity: O(1)");
+            textBox1.Text = results.ToString();
+        }
+
+        //Time Complexity: O(n)
+        //Memory Complexity: O(n)
+        private int UnorderedMap(List<int> _numbers, out double _time)
+        {
+            //Object that will contain unique numbers in original list
+            HashSet<int> map = new HashSet<int>();
+            //Used to determine the time the algorithm took to process list in milliseconds
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            foreach (int num in _numbers)
+            {
+                if (!map.Contains(num))
+                {
+                    map.Add(num);
+                }
+            }
+            stopwatch.Stop();
+            _time = stopwatch.Elapsed.TotalMilliseconds;
+            return map.Count;
+        }
+
+        //Time Complexity: O(n^2)
+        //Memory Complexity: O(1)
+        //Storage Complexity: O(1)
+        private int ConstantMemoryComplexity(List<int> _numbers, out double _time)
+        {
+            //Used to count how many values in list are not unique
+            int notUnique = 0;
+            //Used to determine the time the algorithm took to process list in milliseconds
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            //Retrieves the amount of numbers stored in list
+            int size = _numbers.Count;
+            for (int i = 0; i < size; i++)
+            {
+                //sets num to the ith index of the list
+                int num = _numbers[i];
+                //j begins 1 index greater then i
+                for (int j = i + 1; j < size; j++)
+                {
+                    if (num == _numbers[j])
+                    {
+                        notUnique++;
+                        break;
+                    }
+                }
+            }
+            stopwatch.Stop();
+            _time = stopwatch.Elapsed.TotalMilliseconds;
+            return size - notUnique;
+        }
+
+        //Time Complexity: O(n)
+        //Memory Complexity: O(1)
+        private int ListMethods(List<int> _numbers, out double _time)
+        {
+            //Used to determine the time the algorithm took to process list in milliseconds
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            //Gets distinct elements and convert them into a list again
+            _numbers = _numbers.Distinct().ToList();
+            stopwatch.Stop();
+            _time = stopwatch.Elapsed.TotalMilliseconds;
+            return _numbers.Count;
+        }
+    }
+}
