4c628d3fd9f7f499bd62db79b3a91d7a742071fc
Austin Marino <marino_austin@yahoo.com>
2018-09-18 23:46:25 -0700

    Finished all the code for NotepadApp

---
+++ NotepadApp/NotepadApp/NotepadApp/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿//Austin Marino-11507852
+namespace NotepadApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFromFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFibonacciNumbersfirst50ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFibonacciNumbersfirst100ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 49);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(2235, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(2235, 49);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadFromFileToolStripMenuItem,
+            this.loadFibonacciNumbersfirst50ToolStripMenuItem,
+            this.loadFibonacciNumbersfirst100ToolStripMenuItem,
+            this.saveToFileToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(75, 48);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadFromFileToolStripMenuItem
+            // 
+            this.loadFromFileToolStripMenuItem.Name = "loadFromFileToolStripMenuItem";
+            this.loadFromFileToolStripMenuItem.Size = new System.Drawing.Size(591, 46);
+            this.loadFromFileToolStripMenuItem.Text = "Load From File..";
+            this.loadFromFileToolStripMenuItem.Click += new System.EventHandler(this.loadFromFileToolStripMenuItem_Click);
+            // 
+            // loadFibonacciNumbersfirst50ToolStripMenuItem
+            // 
+            this.loadFibonacciNumbersfirst50ToolStripMenuItem.Name = "loadFibonacciNumbersfirst50ToolStripMenuItem";
+            this.loadFibonacciNumbersfirst50ToolStripMenuItem.Size = new System.Drawing.Size(591, 46);
+            this.loadFibonacciNumbersfirst50ToolStripMenuItem.Text = "Load Fibonacci Numbers (first 50)";
+            this.loadFibonacciNumbersfirst50ToolStripMenuItem.Click += new System.EventHandler(this.loadFibonacciNumbersfirst50ToolStripMenuItem_Click);
+            // 
+            // loadFibonacciNumbersfirst100ToolStripMenuItem
+            // 
+            this.loadFibonacciNumbersfirst100ToolStripMenuItem.Name = "loadFibonacciNumbersfirst100ToolStripMenuItem";
+            this.loadFibonacciNumbersfirst100ToolStripMenuItem.Size = new System.Drawing.Size(591, 46);
+            this.loadFibonacciNumbersfirst100ToolStripMenuItem.Text = "Load Fibonacci Numbers (first 100)";
+            this.loadFibonacciNumbersfirst100ToolStripMenuItem.Click += new System.EventHandler(this.loadFibonacciNumbersfirst100ToolStripMenuItem_Click);
+            // 
+            // saveToFileToolStripMenuItem
+            // 
+            this.saveToFileToolStripMenuItem.Name = "saveToFileToolStripMenuItem";
+            this.saveToFileToolStripMenuItem.Size = new System.Drawing.Size(591, 46);
+            this.saveToFileToolStripMenuItem.Text = "Save to File...";
+            this.saveToFileToolStripMenuItem.Click += new System.EventHandler(this.saveToFileToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 73);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(2235, 930);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2235, 1003);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Austin Marino-11507852";
+            this.Text = "Austin Marino-11507852";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFromFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFibonacciNumbersfirst50ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFibonacciNumbersfirst100ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToFileToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ NotepadApp/NotepadApp/NotepadApp/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+//Austin Marino-11507852
+namespace NotepadApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void LoadText(TextReader sr)
+        {
+            //Make the richTextBox1's Text equal to all of the text in the OpenFileDialog's FileName(filename)
+            richTextBox1.Text = sr.ReadToEnd();
+        }
+        private void loadFromFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Object used to obtain file information
+            using (OpenFileDialog open = new OpenFileDialog())
+            {
+                //Set the Filename of the OpenFileDailog to empty
+                open.FileName = "";
+                //Prompts the user for the file location in their file explorer
+                if (open.ShowDialog(this) == DialogResult.OK)
+                {
+                    //Declare filename as a String equal to the OpenFileDialog's FileName
+                    string fileName = open.FileName;
+                    //Obtains all text within file
+                    TextReader txt = File.OpenText(fileName);
+                    //writes all text to the textbox, overwritting any other text in it
+                    this.LoadText(txt);
+                    //closes the file
+                    txt.Close();
+                }
+                else
+                {
+                    //Unable to open file and grab file path
+                    return;
+                }
+            }
+        }
+
+        private void loadFibonacciNumbersfirst50ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FibonacciTextReader fib = new FibonacciTextReader(50);
+            this.LoadText(fib);
+        }
+
+        private void loadFibonacciNumbersfirst100ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FibonacciTextReader fib = new FibonacciTextReader(100);
+            this.LoadText(fib);
+        }
+
+        private void saveToFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string fileName = "";
+            //Object used to obtain file information
+            using (OpenFileDialog open = new OpenFileDialog())
+            {
+                //Opens a file from the file explorer
+                if (open.ShowDialog(this) == DialogResult.OK)
+                {
+                    //Declare filename as a String equal to the OpenFileDialog's FileName
+                    fileName = open.FileName;
+                }
+                else
+                {
+                    //Unable to open file and grab file path
+                    return;
+                }
+            }
+            //Writes all text currently in the textbox to the file
+            //It overwrites any text already existant in file
+            string text = richTextBox1.Text;
+            File.WriteAllText(fileName, text);
+        }
+    }
+}
